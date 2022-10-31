2a74573dadc4fbce872b09918542257c1df1d7fa
michaelfehr <michael_fehrgmx.de>
2022-10-31 11:03:00 +0100

    updated

--- app/src/main/java/de/androidcrypto/bleserverblessedpart2sdi/SecureDeviceInformationService.java
+++ app/src/main/java/de/androidcrypto/bleserverblessedpart2sdi/SecureDeviceInformationService.java
@@ -8,6 +8,7 @@ import static android.bluetooth.BluetoothGattCharacteristic.PROPERTY_READ;
 import static android.bluetooth.BluetoothGattCharacteristic.PROPERTY_WRITE;
 
 import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
 import android.bluetooth.BluetoothGattService;
 import android.os.Build;
 import android.os.Handler;
@@ -46,9 +47,11 @@ class SecureDeviceInformationService extends BaseService {
     private static final UUID PIN_VERIFICATION_CHARACTERISTIC_UUID = UUID.fromString("0000ff01-0000-1000-8000-00805f9b34fb");
     private static final UUID PIN_VERIFICATION_STATUS_CHARACTERISTIC_UUID = UUID.fromString("0000ff02-0000-1000-8000-00805f9b34fb");
     BluetoothGattCharacteristic pinVerificationStatus = new BluetoothGattCharacteristic(PIN_VERIFICATION_STATUS_CHARACTERISTIC_UUID, PROPERTY_READ, PERMISSION_READ_ENCRYPTED_MITM);
+    private static final UUID PIN_CHANGE_CHARACTERISTIC_UUID = UUID.fromString("0000ff03-0000-1000-8000-00805f9b34fb");
 
     //private byte[] pinStored = new byte[] {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
-    private byte[] pinStored = new byte[] {0x01, 0x02};
+    final private int pinLength = 2; // this is too short for real devices, use a maximum of 10 (bytes) length
+    private byte[] pinStored = new byte[] {0x01, 0x02}; // my default value, get it from the device's flash..
     private byte[] pinToVarify; // the pin to verify needs to be exact 8 bytes long
     private boolean pinVerificationStatusBoolean = false;
 
@@ -72,11 +75,17 @@ class SecureDeviceInformationService extends BaseService {
         //BluetoothGattCharacteristic pinVerification = new BluetoothGattCharacteristic(PIN_VERIFICATION_CHARACTERISTIC_UUID, PROPERTY_WRITE, PERMISSION_WRITE);
         BluetoothGattCharacteristic pinVerification = new BluetoothGattCharacteristic(PIN_VERIFICATION_CHARACTERISTIC_UUID, PROPERTY_WRITE, PERMISSION_WRITE_ENCRYPTED_MITM);
         service.addCharacteristic(pinVerification);
+        pinVerification.addDescriptor(getCharacteristicUserDescriptionDescriptor());
 
-        //BluetoothGattCharacteristic pinVerificationStatus = new BluetoothGattCharacteristic(PIN_VERIFICATION_STATUS_CHARACTERISTIC_UUID, PROPERTY_READ, PERMISSION_READ);
-
+        // this is setup in the header of the class
+        // BluetoothGattCharacteristic pinVerificationStatus = new BluetoothGattCharacteristic(PIN_VERIFICATION_STATUS_CHARACTERISTIC_UUID, PROPERTY_READ, PERMISSION_READ);
         service.addCharacteristic(pinVerificationStatus);
         pinVerificationStatus.addDescriptor(getClientCharacteristicConfigurationDescriptor());
+        pinVerificationStatus.addDescriptor(getCharacteristicUserDescriptionDescriptor());
+
+        BluetoothGattCharacteristic pinChange = new BluetoothGattCharacteristic(PIN_CHANGE_CHARACTERISTIC_UUID, PROPERTY_WRITE, PERMISSION_WRITE_ENCRYPTED_MITM);
+        pinChange.addDescriptor(getCharacteristicUserDescriptionDescriptor());
+        service.addCharacteristic(pinChange);
     }
 
     // changed
@@ -100,27 +109,58 @@ class SecureDeviceInformationService extends BaseService {
     // new
     @Override
     public GattStatus onCharacteristicWrite(@NotNull BluetoothCentral central, @NotNull BluetoothGattCharacteristic characteristic, byte[] value) {
-        // pin needs to be exact 8 bytes long
-        if (value.length != 2) return GattStatus.VALUE_NOT_ALLOWED;
-        BluetoothBytesParser parser = new BluetoothBytesParser(value, ByteOrder.LITTLE_ENDIAN);
-        pinToVarify = parser.getValue().clone();
+
+        if (characteristic.getUuid().equals(PIN_VERIFICATION_CHARACTERISTIC_UUID)) {
+            // pin length is set to 2 in this sample code
+            if (value.length != pinLength) return GattStatus.VALUE_NOT_ALLOWED;
+            BluetoothBytesParser parser = new BluetoothBytesParser(value, ByteOrder.LITTLE_ENDIAN);
+            pinToVarify = parser.getValue().clone();
+        } else if (characteristic.getUuid().equals(PIN_CHANGE_CHARACTERISTIC_UUID)) {
+            // input needs to be exact 2 * 2 bytes long (2 bytes old pin and 2 bytes new pin)
+            if (value.length != (pinLength * 2)) return GattStatus.VALUE_NOT_ALLOWED;
+            BluetoothBytesParser parser = new BluetoothBytesParser(value, ByteOrder.LITTLE_ENDIAN);
+            Timber.i("received bytes %s", BluetoothBytesParser.bytes2String(parser.getValue()));
+            byte[] pinOldNew = parser.getValue().clone();
+            byte[] pinOld = Arrays.copyOf(pinOldNew, pinLength);
+            byte[] pinNew = Arrays.copyOfRange(pinOldNew, pinLength,(pinLength * 2));
+            // check that old pin is correct
+            if (Arrays.equals(pinOld, pinStored)) {
+                Timber.i("old pin equals stored pin");
+                pinStored = pinNew.clone();
+            } else {
+                Timber.i("old pin DOES NOT equals stored pin, aborted");
+            }
+        }
         return super.onCharacteristicWrite(central, characteristic, value);
     }
 
     // new
     @Override
     public void onCharacteristicWriteCompleted(@NonNull BluetoothCentral central, @NonNull BluetoothGattCharacteristic characteristic, @NonNull byte[] value) {
-        Timber.d("pin to verify written %s", Arrays.toString(value));
-        Timber.d("pin stored is         %s", Arrays.toString(pinStored));
-        // verify for debug purposes
-        if (Arrays.equals(pinToVarify, pinStored)) {
-            pinVerificationStatusBoolean = true;
-            Timber.d("pin to verify is correct");
-        } else {
-            pinVerificationStatusBoolean = false;
-            Timber.d("pin to verify is NOT correct");
+        if (characteristic.getUuid().equals(PIN_VERIFICATION_CHARACTERISTIC_UUID)) {
+            Timber.d("pin to verify written %s", Arrays.toString(value));
+            Timber.d("pin stored is         %s", Arrays.toString(pinStored));
+            // verify for debug purposes
+            if (Arrays.equals(pinToVarify, pinStored)) {
+                pinVerificationStatusBoolean = true;
+                Timber.d("pin to verify is correct");
+            } else {
+                pinVerificationStatusBoolean = false;
+                Timber.d("pin to verify is NOT correct");
+            }
+            pinToVarify = new byte[0];
+        } else if (characteristic.getUuid().equals(PIN_CHANGE_CHARACTERISTIC_UUID)) {
+            byte[] pinOldNew = value.clone();
+            byte[] pinOld = Arrays.copyOf(pinOldNew, pinLength);
+            byte[] pinNew = Arrays.copyOfRange(pinOldNew, pinLength,(pinLength * 2));
+            // check that new pin is set
+            if (Arrays.equals(pinNew, pinStored)) {
+                Timber.i("new pin is set");
+                //pinStored = pinNew.clone();
+            } else {
+                Timber.i("new pin was not set");
+            }
         }
-        pinToVarify = new byte[0];
         super.onCharacteristicWriteCompleted(central, characteristic, value);
     }
 
@@ -136,6 +176,24 @@ class SecureDeviceInformationService extends BaseService {
         if (characteristic.getUuid().equals(PIN_VERIFICATION_STATUS_CHARACTERISTIC_UUID)) {
             stopNotifying();
         }
+    }
+
+    @Override
+    public ReadResponse onDescriptorRead(@NotNull BluetoothCentral central, @NotNull BluetoothGattDescriptor descriptor) {
+        if (descriptor.getUuid().equals(CHARACTERISTIC_USER_DESCRIPTION_DESCRIPTOR_UUID)) {
+            if (descriptor.getCharacteristic().getUuid().equals(PIN_VERIFICATION_CHARACTERISTIC_UUID)) {
+                String responseString = "Enter a " + String.valueOf(pinLength) + " byte PIN";
+                return new ReadResponse(GattStatus.SUCCESS, responseString.getBytes(StandardCharsets.UTF_8));
+            } else if (descriptor.getCharacteristic().getUuid().equals(PIN_CHANGE_CHARACTERISTIC_UUID)) {
+                String responseString = "Enter " + String.valueOf(pinLength * 2) + " byte old/new PIN";
+                return new ReadResponse(GattStatus.SUCCESS, responseString.getBytes(StandardCharsets.UTF_8));
+            } else if (descriptor.getCharacteristic().getUuid().equals(PIN_VERIFICATION_STATUS_CHARACTERISTIC_UUID)) {
+                String responseString = "PIN check status";
+                return new ReadResponse(GattStatus.SUCCESS, responseString.getBytes(StandardCharsets.UTF_8));
+            }
+        }
+
+        return super.onDescriptorRead(central, descriptor);
     }
 
     private void notifyPinVerificationStatus() {
